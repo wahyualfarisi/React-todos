2a71a66cef135906705ae1d87c35c21ae06b4d8b
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-08-30 21:01:47 +0700

    create todo ui

--- src/containers/Todo/Todo.js
+++ src/containers/Todo/Todo.js
@@ -17,7 +17,38 @@ class Todo extends Component {
 
     render(){
         let todo = (
-            <h1>My Day</h1>
+            <div>
+                <div className={classes.Todo_header}>
+
+                    <h2>My Day</h2>
+                    <h5>29 August 2020</h5>
+                    <div className={classes.ClearFix}></div>
+
+                    <input type="text" placeholder="Enter List"  />
+                    <button>ADD</button>
+                    <div className={classes.ClearFix}></div>
+                    
+                </div>
+
+                <ul className={classes.Lists}>
+                    <li>Hit the gym Hit the gym <span>x</span> </li>
+                    <li className={classes.Checked}>Pay bills <span>x</span></li>
+                    <li>Meet George <span>x</span></li>
+                    <li>Buy eggs <span>x</span></li>
+                    <li className={classes.Checked}>Read a book <span>x</span></li>
+                    <li>Organize office <span>x</span></li>
+                    <li>Meet George <span>x</span></li>
+                    <li>Buy eggs <span>x</span></li>
+                    <li>Meet George <span>x</span></li>
+                    <li>Buy eggs <span>x</span></li>
+                    <li>Meet George <span>x</span></li>
+                    <li>Buy eggs <span>x</span></li>
+                    <li>Meet George <span>x</span></li>
+                    <li>Buy eggs <span>x</span></li>
+                    <li>Meet George <span>x</span></li>
+                    <li>Buy eggs <span>x</span></li>
+                </ul>
+            </div>
         )
         if(!this.props.auth.isLogin) {
             todo = <h1>Unauthorized 401 </h1>
