65a81f81835e474f3b500aa6e2f05c31ade8a17c
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-09-09 12:12:59 +0700

    update

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const instance = Axios.create({
-    baseURL: 'https://resource-api.alfarisilab.com/api'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : 'https://resource-api.alfarisilab.com/api'
 });
 
 export default instance;
